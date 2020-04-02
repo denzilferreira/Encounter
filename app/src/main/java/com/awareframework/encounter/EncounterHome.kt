b358ff3b198b2a630dcc2bfda473d1053643e78d
Denzil Ferreira <ferreiradenzil@gmail.com>
2020-04-02 14:42:30 +0300

    - added missing fragments, moved stats to self contained fragment - remember the last selected country

--- app/src/main/java/com/awareframework/encounter/EncounterHome.kt
+++ app/src/main/java/com/awareframework/encounter/EncounterHome.kt
@@ -5,70 +5,47 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.database.DatabaseUtils
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.view.ViewStub
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.room.Room
 import com.awareframework.encounter.database.EncounterDatabase
 import com.awareframework.encounter.database.User
 import com.awareframework.encounter.services.EncounterService
+import com.awareframework.encounter.ui.EncountersFragment
+import com.awareframework.encounter.ui.StatsFragment
+import com.awareframework.encounter.ui.SymptomsFragment
 import com.firebase.ui.auth.AuthUI
-import com.github.mikephil.charting.charts.LineChart
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.formatter.IAxisValueFormatter
-import com.github.mikephil.charting.formatter.LargeValueFormatter
-import com.github.mikephil.charting.formatter.ValueFormatter
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.messages.Message
 import com.google.android.gms.nearby.messages.MessageListener
-import com.google.android.material.badge.BadgeDrawable
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.layout_stats.*
 import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.find
 import org.jetbrains.anko.imageBitmap
 import org.jetbrains.anko.uiThread
 import java.io.ByteArrayOutputStream
-import java.text.DecimalFormat
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
-import kotlin.collections.HashSet
-import kotlin.math.log10
-import kotlin.math.pow
 
 class EncounterHome : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
     GoogleApiClient.OnConnectionFailedListener {
 
     lateinit var googleApiClient: GoogleApiClient
-    lateinit var encounterCounter: BadgeDrawable
-    lateinit var db: EncounterDatabase
     lateinit var user: User
 
-    lateinit var tabView: ViewStub
-    lateinit var spread_char : LineChart
-
-    val RC_SIGN_IN = 12345
-    val guiUpdateReceiver = UIUpdate()
+    private val RC_SIGN_IN = 12345
+    private val guiUpdateReceiver = UIUpdate()
 
     companion object {
         val ACTION_NEW_DATA = "ACTION_NEW_DATA"
-        lateinit var mainContainer : View
+        lateinit var mainContainer: View
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,142 +53,25 @@ class EncounterHome : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
         setContentView(R.layout.activity_main)
 
         mainContainer = main_container
-
-        tabView = find(R.id.tab_view_container)
-        tabView.inflate()
+        supportFragmentManager.beginTransaction().replace(R.id.tab_view_container, StatsFragment())
+            .commit()
 
         bottom_nav.setOnNavigationItemSelectedListener { menuItem ->
+            lateinit var selectedFragment: Fragment
             when (menuItem.itemId) {
                 R.id.bottom_stats -> {
-                    tabView.layoutResource = R.layout.layout_stats
-                    tabView.visibility = View.VISIBLE
+                    selectedFragment = StatsFragment()
                 }
                 R.id.bottom_encounters -> {
-                    tabView.layoutResource = R.layout.layout_encounters
-                    tabView.visibility = View.VISIBLE
+                    selectedFragment = EncountersFragment()
                 }
                 R.id.bottom_symptoms -> {
-                    tabView.layoutResource = R.layout.layout_symptoms
-                    tabView.visibility = View.VISIBLE
+                    selectedFragment = SymptomsFragment()
                 }
             }
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.tab_view_container, selectedFragment).commit()
             true
-        }
-
-        country_selector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                count_confirmed.text = getString(R.string.count_number, 0)
-                count_deaths.text = getString(R.string.count_number, 0)
-                count_recovered.text = getString(R.string.count_number, 0)
-            }
-
-            override fun onItemSelected(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                val selectedCountry = country_selector.selectedItem.toString()
-
-                doAsync {
-                    db =
-                        Room.databaseBuilder(
-                            applicationContext,
-                            EncounterDatabase::class.java,
-                            "covid"
-                        ).build()
-
-                    val data = db.StatsDao().getCountryData(selectedCountry)
-                    uiThread {
-                        count_confirmed.text = getString(R.string.count_number, data.last().confirmed)
-                        count_recovered.text = getString(R.string.count_number, data.last().recovered)
-                        count_deaths.text = getString(R.string.count_number, data.last().deaths)
-                    }
-
-                    val weeklyData = db.StatsDao().getWeekly(selectedCountry)
-                    val weeklyCounts = HashMap<Int, ArrayList<Int>>()
-                    weeklyData.moveToFirst()
-                    do {
-                        val weekNumber = weeklyData.getString(weeklyData.getColumnIndex("week")).toInt()
-                        val confirmedAmount = weeklyData.getString(weeklyData.getColumnIndex("confirmed")).toInt()
-                        if (weeklyCounts.containsKey(weekNumber)) {
-                            weeklyCounts[weekNumber]?.add(confirmedAmount)
-                        } else {
-                            weeklyCounts[weekNumber] = ArrayList<Int>().apply {
-                                add(confirmedAmount)
-                            }
-                        }
-                    } while(weeklyData.moveToNext())
-                    weeklyData.close()
-
-                    val weeklyDelta = ArrayList<Float>()
-                    val weeklyTotals = ArrayList<Float>()
-                    for(week in weeklyCounts) {
-                        val min = week.value.min()
-                        val max = week.value.max()
-                        weeklyTotals.add(max!!.toFloat())
-                        weeklyDelta.add(max.toFloat()-min!!.toFloat())
-                    }
-
-                    val plotDataPoints = ArrayList<Entry>()
-                    for (i in 0 until weeklyDelta.size) {
-                        plotDataPoints.add(Entry(scaleCbr(weeklyTotals[i]), scaleCbr(weeklyDelta[i]))) //convert both axis data points to logarithmic scale
-                    }
-
-                    val lineData = LineDataSet(plotDataPoints, selectedCountry)
-                    lineData.setCircleColor(Color.BLUE)
-                    lineData.setDrawCircleHole(true)
-
-                    val dataset = ArrayList<ILineDataSet>()
-                    dataset.add(lineData)
-
-                    val lineChart = LineData(dataset)
-
-                    uiThread {
-                        spread_char = findViewById<LineChart>(R.id.spread_chart)
-
-                        title = getString(R.string.country_trajectory)
-                        spread_char.axisRight.isEnabled = false
-
-                        spread_char.animateX(1500)
-
-                        spread_char.axisLeft.valueFormatter = object : ValueFormatter() {
-                            override fun getFormattedValue(value: Float): String {
-                                val format = DecimalFormat("##.###")
-                                return format.format(unScaleCbr(value))
-                            }
-                        }
-                        spread_char.xAxis.valueFormatter = object : ValueFormatter() {
-                            override fun getFormattedValue(value: Float): String {
-                                val format = DecimalFormat("#.#")
-                                return format.format(unScaleCbr(value))
-                            }
-                        }
-
-                        spread_char.axisLeft.axisMinimum = scaleCbr(10.toFloat())
-                        spread_char.axisLeft.axisMaximum = scaleCbr(1000000.toFloat())
-                        spread_char.axisLeft.setLabelCount(5, true)
-
-                        spread_char.xAxis.axisMinimum = scaleCbr(10.toFloat())
-                        spread_char.xAxis.axisMaximum = scaleCbr(1000000.toFloat())
-                        spread_char.xAxis.setLabelCount(5, true)
-
-                        spread_char.description.isEnabled = false
-                        spread_char.setTouchEnabled(true)
-                        spread_char.setDrawGridBackground(true)
-                        spread_char.isDragEnabled = true
-                        spread_char.setScaleEnabled(true)
-                        spread_char.setPinchZoom(true)
-                        spread_char.data = lineChart
-                        spread_char.invalidate()
-
-                        val legend = spread_char.legend
-                        legend.isEnabled = false
-                    }
-
-                    db.close()
-                }
-            }
         }
 
         val messageListener = object : MessageListener() {
@@ -227,7 +87,7 @@ class EncounterHome : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
         getGoogleClient()
 
         doAsync {
-            db =
+            val db =
                 Room.databaseBuilder(applicationContext, EncounterDatabase::class.java, "covid")
                     .build()
             val users = db.UserDao().getUser()
@@ -250,21 +110,6 @@ class EncounterHome : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
                         .build(), RC_SIGN_IN
                 )
             }
-
-            val countries = db.StatsDao().getCountries()
-            val countryAdapter = ArrayList<String>()
-            for (country in countries) {
-                countryAdapter.add(country.country)
-            }
-            Collections.sort(countryAdapter, String.CASE_INSENSITIVE_ORDER)
-            uiThread {
-                country_selector.adapter =
-                    ArrayAdapter(applicationContext, R.layout.spinner_country, countryAdapter)
-                //TODO: auto select the last selection
-                //country_selector.setSelection(countryIndex, true)
-                //country_selector.dispatchSetSelected(true)
-            }
-
             db.close()
         }
 
@@ -280,14 +125,6 @@ class EncounterHome : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
             .addConnectionCallbacks(this)
             .enableAutoManage(this, this)
             .build()
-    }
-
-    fun scaleCbr(value: Float) : Float {
-        return log10(value)
-    }
-
-    fun unScaleCbr(value: Float) : Float {
-        return 10.0.pow(value.toDouble()).toFloat()
     }
 
     override fun onResume() {
@@ -346,7 +183,11 @@ class EncounterHome : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks,
     class UIUpdate : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             if (intent?.action.equals(ACTION_NEW_DATA)) {
-                Snackbar.make(mainContainer, context?.getString(R.string.data_updated).toString(), Snackbar.LENGTH_SHORT).show()
+                Snackbar.make(
+                    mainContainer,
+                    context?.getString(R.string.data_updated).toString(),
+                    Snackbar.LENGTH_SHORT
+                ).show()
             }
         }
     }
---
+++ app/src/main/java/com/awareframework/encounter/ui/StatsFragment.kt
@@ -0,0 +1,235 @@
+package com.awareframework.encounter.ui
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.content.edit
+import androidx.fragment.app.Fragment
+import androidx.room.Room
+import com.awareframework.encounter.EncounterHome
+import com.awareframework.encounter.R
+import com.awareframework.encounter.database.EncounterDatabase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.LargeValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import kotlinx.android.synthetic.main.fragment_stats.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.support.v4.defaultSharedPreferences
+import org.jetbrains.anko.uiThread
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+import kotlin.math.log10
+import kotlin.math.pow
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class StatsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_stats, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        doAsync {
+            val db = Room.databaseBuilder(context!!, EncounterDatabase::class.java, "covid").build()
+
+            val countries = db.StatsDao().getCountries()
+            val countryAdapter = ArrayList<String>()
+            for (country in countries) {
+                countryAdapter.add(country.country)
+            }
+            Collections.sort(countryAdapter, String.CASE_INSENSITIVE_ORDER)
+            uiThread {
+                country_selector.adapter =
+                    ArrayAdapter(context!!, R.layout.spinner_country, countryAdapter)
+
+                if (defaultSharedPreferences.contains("country")) {
+                    country_selector.setSelection(countryAdapter.indexOf(defaultSharedPreferences.getString("country","")), true)
+                    country_selector.dispatchSetSelected(true)
+                }
+            }
+
+            country_selector.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    count_confirmed.text = getString(R.string.count_number, 0)
+                    count_deaths.text = getString(R.string.count_number, 0)
+                    count_recovered.text = getString(R.string.count_number, 0)
+                }
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val selectedCountry = country_selector.selectedItem.toString()
+
+                    defaultSharedPreferences.edit().putString("country", selectedCountry).apply()
+
+                    doAsync {
+                        val data = db.StatsDao().getCountryData(selectedCountry)
+                        uiThread {
+                            count_confirmed.text =
+                                getString(R.string.count_number, data.last().confirmed)
+                            count_recovered.text =
+                                getString(R.string.count_number, data.last().recovered)
+                            count_deaths.text = getString(R.string.count_number, data.last().deaths)
+                        }
+
+                        val weeklyData = db.StatsDao().getWeekly(selectedCountry)
+                        val weeklyCounts = HashMap<Int, ArrayList<Int>>()
+                        weeklyData.moveToFirst()
+                        do {
+                            val weekNumber =
+                                weeklyData.getString(weeklyData.getColumnIndex("week")).toInt()
+                            val confirmedAmount =
+                                weeklyData.getString(weeklyData.getColumnIndex("confirmed")).toInt()
+                            if (weeklyCounts.containsKey(weekNumber)) {
+                                weeklyCounts[weekNumber]?.add(confirmedAmount)
+                            } else {
+                                weeklyCounts[weekNumber] = ArrayList<Int>().apply {
+                                    add(confirmedAmount)
+                                }
+                            }
+                        } while (weeklyData.moveToNext())
+                        weeklyData.close()
+
+                        val weeklyDelta = ArrayList<Float>()
+                        val weeklyTotals = ArrayList<Float>()
+
+                        var weeksCount = 0
+                        for (week in weeklyCounts) {
+                            weeksCount++
+                            if (weeksCount != weeklyCounts.size-1) { //don't consider the ongoing week data
+                                val min = week.value.min()
+                                val max = week.value.max()
+                                weeklyTotals.add(max!!.toFloat())
+                                weeklyDelta.add(max.toFloat() - min!!.toFloat())
+                            }
+                        }
+
+                        val plotDataPoints = ArrayList<Entry>()
+                        for (i in 0 until weeklyDelta.size) {
+                            plotDataPoints.add(
+                                Entry(
+                                    scaleCbr(weeklyTotals[i]),
+                                    scaleCbr(weeklyDelta[i])
+                                )
+                            ) //convert both axis data points to logarithmic scale
+                        }
+
+                        val lineData = LineDataSet(plotDataPoints, selectedCountry)
+                        lineData.setCircleColor(Color.BLUE)
+                        lineData.setDrawCircleHole(true)
+
+                        val dataset = ArrayList<ILineDataSet>()
+                        dataset.add(lineData)
+
+                        val lineChart = LineData(dataset)
+                        lineChart.setDrawValues(false)
+
+                        uiThread {
+
+                            spread_chart.axisRight.isEnabled = false
+
+                            spread_chart.animateX(1500)
+
+                            val logScale = ArrayList<String>().apply {
+                                add("10") //0
+                                add("100") //1
+                                add("1000") //2
+                                add("10K") //3
+                                add("100K") //4
+                                add("1M") //5
+                            }
+
+                            spread_chart.axisLeft.valueFormatter = object : LargeValueFormatter() {
+                                override fun getFormattedValue(value: Float): String {
+                                    return when (unScaleCbr(value).toInt()) {
+                                        in 1..10 -> logScale[0]
+                                        in 10..100 -> logScale[0]
+                                        in 100..1000 -> logScale[1]
+                                        in 1000..10000 -> logScale[2]
+                                        in 10000..100000 -> logScale[3]
+                                        in 100000..10000000 -> logScale[4]
+                                        in 1000000..1000000000 -> logScale[5]
+                                        else -> "1M+ :(" //OMG
+                                    }
+                                }
+                            }
+                            spread_chart.xAxis.valueFormatter = object : LargeValueFormatter() {
+                                override fun getFormattedValue(value: Float): String {
+                                    return when (unScaleCbr(value).toInt()) {
+                                        in 1..10 -> logScale[0]
+                                        in 10..100 -> logScale[0]
+                                        in 100..1000 -> logScale[1]
+                                        in 1000..10000 -> logScale[2]
+                                        in 10000..100000 -> logScale[3]
+                                        in 100000..10000000 -> logScale[4]
+                                        in 1000000..1000000000 -> logScale[5]
+                                        else -> "1M+ :(" //OMG
+                                    }
+                                }
+                            }
+
+                            spread_chart.axisLeft.axisMinimum = scaleCbr(10.toFloat())
+                            spread_chart.axisLeft.axisMaximum = scaleCbr(1000000.toFloat())
+                            spread_chart.axisLeft.isGranularityEnabled = true
+                            spread_chart.axisLeft.setLabelCount(5, true)
+
+                            spread_chart.xAxis.axisMinimum = scaleCbr(10.toFloat())
+                            spread_chart.xAxis.axisMaximum = scaleCbr(1000000.toFloat())
+                            spread_chart.xAxis.isGranularityEnabled = true
+                            spread_chart.xAxis.setLabelCount(5, true)
+                            spread_chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+                            spread_chart.description.isEnabled = false
+                            spread_chart.setTouchEnabled(true)
+                            spread_chart.setDrawGridBackground(true)
+                            spread_chart.isDragEnabled = true
+                            spread_chart.setScaleEnabled(true)
+                            spread_chart.setPinchZoom(true)
+                            spread_chart.data = lineChart
+                            spread_chart.invalidate()
+
+                            val legend = spread_chart.legend
+                            legend.isEnabled = false
+                        }
+
+                        db.close()
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Convert value to log10
+     */
+    fun scaleCbr(value: Float): Float {
+        return log10(value)
+    }
+
+    /**
+     * Undo log10 back to value
+     */
+    fun unScaleCbr(value: Float): Float {
+        return 10.0.pow(value.toDouble()).toFloat()
+    }
+}
